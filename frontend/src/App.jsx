7075f2f7cfe44f7e654c3f4141b22eaf80e6cfa6
Mazhar Niaz <mazharniaz1997@gmail.com>
2026-06-12 16:10:53 +0500

    Initial frontend setup with React js, Tailwind CSS, and its configuration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-6 text-2xl">
+      Tailwind Working!
+    </div>
+  )
+}
+
+export default App
